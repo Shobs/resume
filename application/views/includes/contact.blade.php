d98deb96b6c3f8ffefc12887b57d9fa01403bdc2
Jean Marcellin <marcellinja@gmail.com>
2013-02-06 12:29:29 +0100

    Fixes on mailto

--- application/views/includes/contact.blade.php
+++ application/views/includes/contact.blade.php
@@ -5,7 +5,7 @@
 	<p>{{HTML::link('maps.html?iframe=true&amp;width=600&amp;height=400', 'View Google Map', array('rel' => 'prettyPhoto'));}}
 	</p>
 	<div id="note"></div>
-	{{Form::open('MAILTO:marcellinja@gmail.com', 'post');}}
+	{{Form::open('mailto:marcellinja@gmail.com', 'post');}}
 		<!-- Change your email id in the config.php file -->
 		<div class="field">
 			{{Form::label('name', 'Name *', array('class' => 'label', 'for' => 'name'));}}
