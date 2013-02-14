1b0b10d259e9bcff8fddc5f48c2de1ba6516813a
Jean Marcellin <marcellinja@gmail.com>
2013-02-14 19:06:24 +0100

    Added print.css and misc modifications

--- application/views/includes/header.blade.php
+++ application/views/includes/header.blade.php
@@ -6,7 +6,7 @@
 		+32 495 12 21 50<br>
 		{{HTML::mailto('marcellinja@gmail.com', 'marcellinja@gmail.com');}}<br>
 		<!--For additional security it is advisable to encrypt this email id using http://hivelogic.com/enkoder/form ^_^_^_^_^ -->
-		{{HTML::link('#', 'www.domainname.com', array('class' => 'external'));}}
+		{{HTML::link('#', 'www.jeanmarcellin.net', array('class' => 'external'));}}
 	</p>
 	<!-- end / #label -->
 </div>
